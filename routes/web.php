291478c464c3e4f143f6af46e531a7e78e035308
Riaz Laskar <riazcool77@gmail.com>
2024-07-31 16:32:33 +0530

    initial frontend pages

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Livewire\AboutUsPage;
+use App\Livewire\ContactUsPage;
+use App\Livewire\HomePage;
+use App\Livewire\ProductDetailsPage;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', HomePage::class);
+Route::get('/shop-details', ProductDetailsPage::class);
+Route::get('/contact', ContactUsPage::class);
+Route::get('/about', AboutUsPage::class);
